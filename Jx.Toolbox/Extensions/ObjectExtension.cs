5e8fbf107bcbf2f898f7239b64f2b096d04dbfc6
jx <jx@jvxiang.com>
2022-09-16 17:02:42 +0800

    修改获取设置属性的方式

--- Jx.Toolbox/Extensions/ObjectExtension.cs
+++ Jx.Toolbox/Extensions/ObjectExtension.cs
@@ -1,13 +1,34 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Jx.Toolbox.Extensions
 {
     public static class ObjectExtension
     {
-        public static void SetProperty(this object obj, string name, object value)
+
+        /// <summary>
+        /// 获取属性
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="bindingFlags"></param>
+        /// <returns></returns>
+        public static PropertyInfo[] GetProperties(this object obj, BindingFlags bindingFlags = BindingFlags.Public)
+        {
+            return obj.GetType().GetProperties(bindingFlags);
+        }
+        
+        /// <summary>
+        /// 设置属性
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <param name="bindingFlags"></param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void SetProperty(this object obj, string name, object value, BindingFlags bindingFlags = BindingFlags.Public)
         {
             var t = obj.GetType();
-            var p = t.GetProperty(name);
+            var p = t.GetProperty(name, bindingFlags);
 
             if (p == null)
             {
