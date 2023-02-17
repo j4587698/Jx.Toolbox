46463f941f89e38c03b849d4bc728cf166a8b691
jx <jx@jvxiang.com>
2023-02-17 10:01:45 +0800

    添加Os系统类型

---
+++ Jx.Toolbox/Utils/ProcessUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Jx.Toolbox.Utils
+{
+    public class ProcessUtil
+    {
+        public static void KillProcess(string processName)
+        {
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.Kill();
+            }
+        }
+        
+        public static void KillProcess(int processId)
+        {
+            Process.GetProcessById(processId).Kill();
+        }
+        
+        public static void StartProcess(string fileName, string arguments)
+        {
+            Process.Start(fileName, arguments);
+        }
+    }
+}
