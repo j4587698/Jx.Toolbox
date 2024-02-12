ac79c8871ce17777c1b40f4d6007a3f0b7b25d66
jx <jx@jvxiang.com>
2024-02-12 21:44:12 +0800

    添加LongExtension

---
+++ Jx.Toolbox.Test/LongTest.cs
@@ -0,0 +1,20 @@
+﻿using Jx.Toolbox.Extensions;
+
+namespace Jx.Toolbox.Test;
+
+public class LongTest
+{
+    [Fact]
+    public void ToSizeString_Ok()
+    {
+        int fileSizeInBytes = 12345678;
+        Assert.Equal("11.77 MB", fileSizeInBytes.ToSizeString());
+    }
+
+    [Fact]
+    public void ToTimeString_Ok()
+    {
+        int totalSeconds = 3661;
+        Assert.Equal("01:01:01", totalSeconds.ToTimeString());
+    }
+}
